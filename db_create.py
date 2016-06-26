1c9dd4653c018fd8d77fdecfcbbe27fc06e2884a
Randall Spears <rspears690@gmail.com>
2016-06-25 23:43:48 -0400

    add migrations

--- db_create.py
+++ db_create.py
@@ -7,6 +7,8 @@ db.create_all()
 # INSERT
 db.session.add(BlogPost("Good", "Gay homo bitch"))
 db.session.add(BlogPost("well", "Im gay"))
+db.session.add(BlogPost("Flask", "discoverflask.com"))
+db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
 # commit the changes
 db.session.commit()
