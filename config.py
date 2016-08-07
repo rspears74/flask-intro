30b71e92b5634cdba86fc4dbc4bdaaf3a1af6618
Randall Spears <rspears690@gmail.com>
2016-08-07 16:19:48 -0400

    made secret_key config variable part of os environment variable instead of directly inside config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = '\x97\x0c\x9e\xaa\xea\x99\xeeN_\xeaK\xdeV\xf8\x1b!?\x08\xa1\xe0\xca`%\xe2'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
