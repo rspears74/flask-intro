b009277e150349319f4c5aae95c63b7a703911f3
Randall Spears <rspears690@gmail.com>
2016-08-06 23:52:46 -0400

    try again to solve the heroku shit by removing decode(utf-8) from hash)

--- project/models.py
+++ project/models.py
@@ -31,7 +31,7 @@ class User(db.Model):
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password = bcrypt.generate_password_hash(password)
 
     def is_authenticated(self):
         return True
